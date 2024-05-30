8694d1feb1b575ece624512cef5b23632e2d982c
GeekyDeaks <sdeaks@gmail.com>
2024-05-30 14:02:14 +0100

    add users within a transaction

--- xfdev/example-users.php
+++ xfdev/example-users.php
@@ -4,6 +4,8 @@ $dir = '/var/www/html';
 require ($dir . '/src/XF.php');
 XF::start($dir);
 
+\XF::app()->db()->beginTransaction();
+
 $group = \XF::em()->create('XF:UserGroup');
 $group->title = 'Premium Members';
 $group->banner_css_class = 'userBanner userBanner--orange';
@@ -22,14 +24,18 @@ $users = [
 
 $userRepo = \XF::app()->repository('XF:User');
 
+# took hints for this from XF/Install/Helpler:createInitialUser()
+
 foreach ($users AS $u) {
     $user = $userRepo->setupBaseUser();
     $user->username = $u['username'];
-    $user->email = $u['username'] .'@example.com';
+    $user->email = strtolower($u['username']) .'@example.com';
     $user->set('secondary_group_ids',$u['groups']);
     $auth = $user->getRelationOrDefault('Auth');
     $auth->setPassword('overtake');
     $user->save();
 }
+
+\XF::app()->db()->commit();
 
 
